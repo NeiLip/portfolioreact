6a6bc7b170104c073b5f71fea1d2e2dd6746812c
Neil <neil2193@gmail.com>
2020-09-23 18:30:35 +0300

    Updates

---
+++ precache-manifest.4519cd5364169c3be6097a54d31da7de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b77ac868b1df5bd4e8c2",
+    "url": "/portfolioreact/static/css/main.7021e858.chunk.css"
+  },
+  {
+    "revision": "b77ac868b1df5bd4e8c2",
+    "url": "/portfolioreact/static/js/main.b77ac868.chunk.js"
+  },
+  {
+    "revision": "276d8b68e680dccf5648",
+    "url": "/portfolioreact/static/js/runtime~main.276d8b68.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolioreact/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "248deae96c28919374630bc724fa3ddd",
+    "url": "/portfolioreact/index.html"
+  }
+];
